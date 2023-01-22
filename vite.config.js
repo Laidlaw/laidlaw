ff015c78f88b6cef6ece23d246e9b97b12af7b16
Alan Laidlaw <alanlaidlaw@gmail.com>
2023-01-21 22:45:40 -0500

    seeking parity w swyxkit pt 1. update packages. next up: utterances and search.

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ const config = {
 	plugins: [ssp(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		fs: {
+			// https://vitejs.dev/config/server-options.html#server-fs-allow
+			// allows importing readme for About page
+			allow: ['..']
+		}
 	}
 };
 
